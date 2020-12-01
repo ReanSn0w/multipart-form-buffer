1450b35012c67f146af25da670df65a7d09adc95
Dmitriy Papkov <papkovda@me.com>
2020-12-01 12:31:34 +0300

    Update MultipartFormBufferTests.swift
    Тест заполнения буфера информацией путем записи пары из ключа и значения

--- Tests/MultipartFormBufferTests/MultipartFormBufferTests.swift
+++ Tests/MultipartFormBufferTests/MultipartFormBufferTests.swift
@@ -2,14 +2,22 @@ import XCTest
 @testable import MultipartFormBuffer
 
 final class MultipartFormBufferTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MultipartFormBuffer().text, "Hello, World!")
+    func keyValueTest() {
+        let name = "test-name"
+        let value = "test-value"
+        
+        let buffer = MultipartFormBuffer()
+        buffer.addValue(name: name, value: value)
+        
+        let bufferedData = buffer.get()
+        let bufferedString = String.init(data: bufferedData, encoding: .utf8)
+        
+        XCTAssert(bufferedString != nil)
+        XCTAssert(bufferedString?.contains(value) ?? false)
+        XCTAssert(bufferedString?.contains("Content-Disposition: form-data; name=\"\(name)\"") ?? false)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("keyValueTest", keyValueTest),
     ]
 }
